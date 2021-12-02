c88a5191d0498466ffefd4ac1463328647d85854
Noah Emke <noahremke@gmail.com>
2021-12-02 07:01:25 -0500

    Fixed load order to prevent null reference errors.

--- js/game.js
+++ js/game.js
@@ -4,27 +4,27 @@
 /* TODO: Dynamically size the window and scale the game toy the size of the 
 browser window. */
 const game = new PIXI.Application({
-    width: 600,
-    height: 600
+	width: 600,
+	height: 600
 });
-document.body.appendChild(game.view);
 //#endregion
 
-//#region Image loading
-// TODO: Load and use textures in place of primitive shapes.
-game.loader.
-    add([
-        
-    ]);
-game.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
-game.loader.onComplete.add(Setup);
-game.loader.load();
-//#endregion
+window.onload = () => {
+	document.body.appendChild(game.view);
+	
+	//#region Image loading
+	// TODO: Load and use textures in place of primitive shapes.
+	game.loader.
+	add([
+		
+	]);
+	game.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
+	game.loader.onComplete.add(Setup);
+	game.loader.load();
+	//#endregion
+};
 
 //#region Variables
-const sceneWidth = game.view.width;
-const sceneHeight = game.view.height;
-
 let playerBullets = [];
 let enemyBullets = [];
 let enemies = [];
